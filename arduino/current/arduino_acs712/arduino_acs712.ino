3ef76aff5f34a537b16e14c3aa42a6d027d3f38b
Oleg Kovalichin <oleg@bromleysat.com>
2025-04-16 12:25:09 +0100

    Update arduino_acs712.ino

--- arduino/current/arduino_acs712/arduino_acs712.ino
+++ arduino/current/arduino_acs712/arduino_acs712.ino
@@ -1,30 +1,31 @@
 
 #define SENSOR_MID_POINT 2.5
-#define VOLTS_PER_AMP 0.185
+#define VOLTS_PER_AMP 0.185  //0.185 => 5A  0.100 => 20A 0.066 => 30A
 
 void setup() {
   Serial.begin(9600);
   pinMode(A0, INPUT);
 }
- 
-void loop() {
-  float sensorValue = readAnalogWithMultisampling(A0, 200.0);
-  //convert the sensor value to current
-  float current = (SENSOR_MID_POINT - fmap(sensorValue, 0.0, 1023.0, 0.0, 5.0)) / VOLTS_PER_AMP;
 
-  //print current value
-  Serial.print("SensorValue:");
-  Serial.println(sensorValue);
+void loop() {
+  float current = (SENSOR_MID_POINT - readVoltageWithMultiSampling(A0, 200.0)) / VOLTS_PER_AMP;
+
   Serial.print("Current:");
   Serial.println(current);
 
-  delay(100); //delay between measurements
+  delay(100);  //delay between measurements
+}
+
+//reading an average analog value and then converting it to voltage from 0 to 5V
+float readVoltageWithMultiSampling(uint8_t pin, float sampleSize) {
+  float sensorValue = readAnalogWithMultiSampling(pin, sampleSize);
+  return fmap(sensorValue, 0.0, 1023.0, 0.0, 5.0);
 }
 
 //function to read analog values from a specific pin
 //an average value is calculated based on the sampleSize parameter
 //3ms delay between sample reads
-float readAnalogWithMultisampling(uint8_t pin, float sampleSize) {
+float readAnalogWithMultiSampling(uint8_t pin, float sampleSize) {
   float analogValue = 0.0, total = 0.0;
 
   for (int x = 0; x < sampleSize; x++) {
@@ -33,6 +34,7 @@ float readAnalogWithMultisampling(uint8_t pin, float sampleSize) {
     delay(3);
   }
   float sensorValue = total / sampleSize;
+  return sensorValue;
 }
 
 //function to map sensor value range to the voltage range
